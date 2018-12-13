3f9b0a35f7360db1e71173ae2c892e80bc0ccb4f
baotran810 <baotranduong810@gmail.com>
2018-12-13 14:59:02 +0700

    Add user following
    Add password reset
    
    final

---
+++ db/migrate/20181129035847_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+
+      t.timestamps
+    end
+    add_index :relationships, :follower_id
+    add_index :relationships, :followed_id
+    add_index :relationships, [:follower_id, :followed_id], unique: true
+  end
+end
